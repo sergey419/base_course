20a8c35da33f19914b4c1b391c737527133112ea
sergey419 <sergejnazarkin69@gmail.com>
2024-10-20 09:10:29 +0000

    ouhouhtt

--- task_2.py
+++ task_2.py
@@ -1,16 +1,11 @@
-x = ('Hello, World!')
-
+x = 'Hello World'
 print(type(x))
 
-x = (3+4)
-
+x = 3 + 4
 print(type(x))
 
-x = ( 3 / 4)
-
+x = 3/4
 print(type(x))
 
-x = ([1, 2, 5, 10, 100])
-
+x = [1,2,5,10,100]
 print(type(x))
-
