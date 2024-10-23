0c54dd7db85dd25ce479500c29037ccc64c39e58
sergey419 <sergejnazarkin69@gmail.com>
2024-10-23 16:29:33 +0000

    лабаyh897yt8yy

--- task_2.py
+++ task_2.py
@@ -1,5 +1,11 @@
-a = task_2.tan(35 * task_2.pi **2 )
-print(a)
- 
 
-     
+import numpy as np
+h = 100
+alpha = 45
+beta = 35
+gravity_constant = 9,8
+V=(((gravity_constant*h*np.tan(beta)**2)/(2*np.cos(alpha)**2)*(1 -np.tan(beta)*np.tan(alpha))))**0.5
+print(V)
+
+
+
---
+++ task_3.massive.py
@@ -0,0 +1,6 @@
+import task_3 as t3
+import numpy as np
+# Массив со столбцами t, x, y
+
+
+
