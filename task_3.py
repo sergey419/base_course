c5d2d07bfaebe8c9646ca0954c1d326986264e05
sergey419 <sergejnazarkin69@gmail.com>
2024-10-30 15:25:03 +0000

    сделал 3 задание

--- task_3.py
+++ task_3.py
@@ -1,3 +1,19 @@
+import numpy as np
+from lec_3_my_module import gravity_constant as g
+
+t = float(input('Введите значение '))
+x0 = float(input('Введите значение '))
+y0 = float(input('Введите значение '))
+V0x = float(input('Введите значение '))
+
+x = x0 + V0x*t
+y = y0 + V0x*t - (g*t**2)/2
+
+coord = np.column_stack((t, x, y))
+print(coord)
+
+
+
 
 
 
