eaa0551eced115e8ce908631eb10cc876a3c6eed
sergey419 <sergejnazarkin69@gmail.com>
2024-11-20 15:26:05 +0000

    сделал 4 задание

--- task_4.py
+++ task_4.py
@@ -1,7 +1,13 @@
 import numpy as np
-
+N = int(input('Введите элемент: '))
+M = int(input('Введите элемент: '))
 trigonometry_array = np.zeros((N, M))
-
-
-
+for i in range(N):
+    for j in range(M):
+        summ = np.sin(N * i + M *j + 1)
+        if summ < 0:
+            trigonometry_array[i, j] = 0
+        else:
+            trigonometry_array[i, j] = summ
+print(trigonometry_array)
 
